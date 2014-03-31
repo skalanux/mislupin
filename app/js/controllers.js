376061a6aa783b9885eb019b5a7c9e55165aa157
Juan Manuel Schillaci <jmschillaci@gmail.com>
2014-03-30 22:51:23 -0300

    Added filter by character

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -19,13 +19,14 @@ $scope.planitos = [
 
 angular.module('busquedaApp.controllers').controller('BusquedaHistorietasCtrl', ['$scope', function($scope) {
 
-$scope.planitos = [
-{'descripcion': 'Dibujos Animados - Gordi saltando la soga',
-'numero': '123'},
-{'descripcion': 'Un receptor muy simple',
-'numero': '201'},
-{'descripcion': 'Una radito experimental',
-'numero': '134'}
+$scope.historietas = [
+{'nombre': 'Problemas en el cosmodromo',
+'numero': '145',
+ 'personaje': 'Bicho y gordi' },
+{'nombre': 'Hombres de marte',
+'numero': '345',
+ 'personaje': 'Resorte y el profe'
+}
 ];
 
 
