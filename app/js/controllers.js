15a013ba3c0dcbae4a0be9d1d8c25436c0d35580
Juan Manuel Schillaci <jmschillaci@gmail.com>
2014-03-30 22:35:01 -0300

    Added BusquedaHistorietasCtrl

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,22 @@
 
 /* Controllers */
 
-angular.module('busquedaApp.controllers', [])
-  .controller('BusquedaPlanitosCtrl', ['$scope', function($scope) {
+angular.module('busquedaApp.controllers', []);
+
+angular.module('busquedaApp.controllers').controller('BusquedaPlanitosCtrl', ['$scope', function($scope) {
+$scope.planitos = [
+{'descripcion': 'Dibujos Animados - Gordi saltando la soga',
+'numero': '123'},
+{'descripcion': 'Un receptor muy simple',
+'numero': '201'},
+{'descripcion': 'Una radito experimental',
+'numero': '134'}
+];
+
+
+}]);
+
+angular.module('busquedaApp.controllers').controller('BusquedaHistorietasCtrl', ['$scope', function($scope) {
 
 $scope.planitos = [
 {'descripcion': 'Dibujos Animados - Gordi saltando la soga',
@@ -16,3 +30,4 @@ $scope.planitos = [
 
 
 }]);
+
